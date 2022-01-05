c8133bb4a9df5f2d1b7ad6da6e18f78648046e71
toveseger <tove.seger@outlook.com>
2022-01-05 15:01:22 +0100

    bg done

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>   
-    <Navigation/> 
+  <div class="index-bg">   
+    <Navigation/>    
+    <SpecialOffer/>
     <Footer/>
   </div>
 </template>
@@ -9,12 +10,18 @@
 import Navigation from "../src/components/Navigation.vue"
 import Cart from "../src/components/Cart.vue"
 import Footer from "../src/components/Footer.vue"
+import SpecialOffer from "../src/components/SpecialOffer.vue"
   export default {
-    components: { Navigation, Cart, Footer }
+    components: { Navigation, Cart, Footer, SpecialOffer }
 }
 </script>
 
 <style scoped>
-
+.index-bg{
+  background-image: url("../src/assets/img/buddhaBeige.jpg");
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+}
 
 </style>
