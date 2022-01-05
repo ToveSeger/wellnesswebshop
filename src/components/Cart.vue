31a6e8429ce7a33536d64ce87c746d3661e42afe
toveseger <tove.seger@outlook.com>
2022-01-05 11:04:57 +0100

    nav done

---
+++ src/components/Cart.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="cartContainer">
+        <img class="cart" src="../../src/assets/img/cart.jpg" alt="Cart">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Cart"          
+}
+
+</script>
+
+<style scoped>
+    .cart{
+        height:2em;    
+    }
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="footer-bg">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+ .footer-bg{
+        height: 7em;
+        width: 100vw;
+        background-color: #EAF4F7;
+    }
+
+</style>
