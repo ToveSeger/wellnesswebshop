29f218af7cf49f3aaf20428b53dcfe82daac8818
toveseger <tove.seger@outlook.com>
2022-01-11 16:07:34 +0100

    testing _slug solution for products

---
+++ src/components/productButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <!-- <div @click="$router.push(`{name:'product', params:{productId: ${productId}}}`)">Read more</div> -->
+      <NuxtLink :to="product">read more</NuxtLink>
+    </div>
+</template>
+
+<script>
+import ChosenProduct from "./ChosenProduct.vue";
+    export default {
+    props: ['productId'],
+    components: { ChosenProduct }
+}
+</script>
+
+<style scoped>
+
+</style>
