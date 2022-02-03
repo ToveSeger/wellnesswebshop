822b8b9cf84eb545d8dde592d7fbe1280094030b
toveseger <tove.seger@outlook.com>
2022-02-03 08:42:02 +0100

    small change in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: true, 
 
+  //Needed for deployment to Netlify
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'wellnesswebshop',
