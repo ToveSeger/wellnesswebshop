fc0d3b3696a39036cf6ffb564a736636e0f37972
toveseger <tove.seger@outlook.com>
2022-02-03 13:54:20 +0100

    migrationChanges

--- prisma/migrations/20220124095117_change_in_customer_table/migration.sql
+++
@@ -1,7 +0,0 @@
--- DropIndex
-DROP INDEX `Customer_road_key` ON `customer`;
-
--- AlterTable
-ALTER TABLE `customer` MODIFY `phone` INTEGER NOT NULL,
-    MODIFY `road` VARCHAR(100) NOT NULL,
-    MODIFY `zip_code` INTEGER NOT NULL;
--- prisma/migrations/20220124134003_removed_field_in_order/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `product_cost` on the `order` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `order` DROP COLUMN `product_cost`;
