16082c0dd5344f7064b5539d9813b3c53fe540b2
toveseger <tove.seger@outlook.com>
2022-02-10 08:12:00 +0100

    small styling change

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,10 @@
                     <ProductCardSlim :product="product"/>              
             </article>
             <NuxtLink type="button" to="/outlet" class="btn btn-light">
-            Take me to the outlet!
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
+            <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/>
+            </svg>
+            <p>Take me to the outlet!</p>
             </NuxtLink>
           </div>
     </main>  
@@ -40,6 +43,7 @@ import ProductCardSlim from "../src/components/ProductCardSlim.vue";
 <style scoped>
 
 .container{
+  font-family: 'Roboto Condensed', sans-serif;
   margin-top:2em;
   height:100vh;
   margin-bottom:10em;
@@ -58,14 +62,13 @@ import ProductCardSlim from "../src/components/ProductCardSlim.vue";
 
 .btn-light{
   color: #177585;
-  max-height:4em;
-  max-width:19em;  
+  max-height:5em;
+  max-width:15em;  
   margin-top:6em; 
-  margin-left:1.5em;
-  font-weight: bold;
-  font-size:clamp(0.8rem, 0.8rem + 0.3vw, 1.5rem);
+  margin-left:1.7em;
+  font-weight: 700;
+  font-size:clamp(0.8rem, 1rem + 0.3vw, 1.5rem);
   border: 0.4em solid  #177585;
-  padding-top:0.8em;
 }
 
 .card{
