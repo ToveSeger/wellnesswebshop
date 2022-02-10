088389a8e24596c68aec92adeee0024355e3d605
toveseger <tove.seger@outlook.com>
2022-02-10 08:20:47 +0100

    change of cart icon in mobile version

--- src/components/CartDropDown.vue
+++ src/components/CartDropDown.vue
@@ -1,3 +1,4 @@
+//Technical Documentation 7.9.6
 <template>
     <main class="container">
         <h4>My cart</h4>
--- src/components/MobileNavigation.vue
+++ src/components/MobileNavigation.vue
@@ -18,9 +18,7 @@
                 <NuxtLink to="/aromalamps" class="nav-link">Aroma Lamps</NuxtLink>                  
                  <div class="divider"> | </div>
                 <NuxtLink to="/cart"> <img class="cart"/>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
-                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                </svg>
+                <img class="cart" src="../../src/assets/img/cart_rounded.jpg" alt="cart"> 
                 </NuxtLink>
                 </div>              
             </div>                        
@@ -77,5 +75,10 @@ import Search from './Search.vue'
         font-size: x-large;
         transform: rotate(90deg); 
     }
+
+     .cart{
+         width:4em;
+         border-radius: 50%;
+     }
     
 </style>
